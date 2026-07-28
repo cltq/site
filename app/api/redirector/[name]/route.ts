12d1ca7f67d482e7010aa6493134ea82cb91291a
cltq <shapebrabra@gmail.com>
2026-07-28 20:51:18 +0700

    feat: add additional social links to redirector API

--- app/api/redirector/[name]/route.ts
+++ app/api/redirector/[name]/route.ts
@@ -3,6 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 const links: Record<string, string> = {
   github: "https://github.com/cltq",
   discord: "https://discord.com/users/969088519161139270",
+  haunt: "https://haunt.gg/fumi",
+  easydonate: "https://easydonate.com/ivnfumi",
+  instagram: "https://www.instagram.com/lnfumi._",
 };
 
 export async function GET(
